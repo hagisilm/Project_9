5d0d40371cd547962e852218df87cd98f62a2965
hagisilm <catchwall3203@naver.com>
2023-02-18 17:34:21 +0900

    test

--- src/main/java/com/example/Project9/Project9Application.java
+++ src/main/java/com/example/Project9/Project9Application.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Project9Application {
 
 	public static void main(String[] args) {
+		System.out.println(" ");
 		SpringApplication.run(Project9Application.class, args);
 	}
+
 
 }
